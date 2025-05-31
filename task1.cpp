789a3ee842a3bc04400f7eec7f6db475e2443296
MueezKhalid <mueez.m.khalid@gmail.com>
2025-05-31 17:53:12 +0500

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,60 +1,57 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void input(char name[10][20], int marks[10][5]) {
+    cout << "ENTER THE NAME AND MARKS OF 5 SUBJECTS" << endl << endl;
 
-	cout << "ENTER THE NAME AND MARKS OF % SUBJECT" << endl << endl << endl;
+    int result[10][2]; 
+    cin.ignore(); 
 
-	int result[10][2], sum = 0, average, top_score = -9999;
-	for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
+        int sum = 0, average, top_score = -9999;
 
-		cout << "ENTER NAME OF " << i + 1 << " STUDENT: ";
-		cin.ignore();
-		cin.getline(name[i], 20);
-		cout << endl;
+        cout << "ENTER NAME OF STUDENT " << i + 1 << ": ";
+        cin.getline(name[i], 20); 
+        cout << endl;
 
-		for (int j = 0; j < 5; j++) {
-			cout << "ENETER THE MARKS OF THE " << j + 1 << " SUBJECT: ";
-			cin >> marks[i][j];
-			sum += marks[i][j];
-		}
-		average = sum / 5;
+        for (int j = 0; j < 5; j++) {
+            cout << "ENTER MARKS OF SUBJECT " << j + 1 << ": ";
+            cin >> marks[i][j];
+            sum += marks[i][j];
+            if (marks[i][j] > top_score) {
+                top_score = marks[i][j];
+            }
+        }
 
-		for (int j = 0; j < 5; j++) {
-			if (top_score < marks[i][j]) {
-				top_score = marks[i][j];
-			}
-		}
+        average = sum / 5;
+        result[i][0] = average;
+        result[i][1] = top_score;
+        cout << endl;
 
-		result[i][0] = average;
-		result[i][1] = top_score;
+        cin.ignore(); 
+    }
 
+    cout << "\nNAME            SUBJECT 1  SUBJECT 2  SUBJECT 3  SUBJECT 4  SUBJECT 5  AVERAGE   TOP_MARKS\n";
 
-		cout << endl;
-	}
-	cout << "NAME  SUBJECT 1  SUBJECT 2  SUBJECT 3  SUBJECT 4  SUBJECT 5  AVERAGE  TOP_MARKS" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout.width(15);
+        cout << left << name[i];
 
-	for (int i = 0; i < 10; i++) {
-
-		for (int j = 0; name[i][j] != '\0'; j++) {
-			cout << name[i][j];
-		}
-		cout << "       ";
-		for (int j = 0; j < 5; j++) {
-			cout << marks[i][j] << "        ";
-		}
-		cout << result[i][0] << "        ";
-		cout << result[i][1] << "        ";
-		cout << endl;
-	}
-
+        for (int j = 0; j < 5; j++) {
+            cout.width(10);
+            cout << marks[i][j];
+        }
+        cout.width(10);
+        cout << result[i][0];
+        cout.width(10);
+        cout << result[i][1];
+        cout << endl;
+    }
 }
 
 int main() {
-
-	char name[4][20];
-	int marks[4][5];
-	input(name, marks);
-
-	return 0;
+    char name[10][20];
+    int marks[10][5];
+    input(name, marks);
+    return 0;
 }
